90c630e8a0f3cbcd2773256117819c38eb8208ae
RAD-AgileDevOps <26099202+RAD-AgileDevOps@users.noreply.github.com>
2026-03-13 16:36:52 -0400

    Add delay to view logs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 const secretKey = process.env.VESSEL_KEY || "NO_KEY_FOUND";
-
 console.log("--- SYSTEM INITIALIZED ---");
 console.log("Accessing the Vessel...");
-console.log(`Security Clearance: ${secretKey}`);
+console.log("Security Clearance: " + secretKey);
+
+// Keep the vessel alive for 60 seconds
+setTimeout(() => {
+  console.log("Mission Complete. Powering down.");
+}, 60000);
